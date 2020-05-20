dd82d6be883bba1b3082bd714bdc4892e3c4dd45
Alex Iribarren <Alex.Iribarren@cern.ch>
2020-05-20 18:27:49 +0200

    Make it wide

--- snow-fixer.user.js
+++ snow-fixer.user.js
@@ -1,21 +1,17 @@
 // ==UserScript==
-// @name       Service Now Fixer
-// @namespace  http://cern.ch
-// @version    1.0
+// @name         Service Now Fixer
+// @namespace    http://cern.ch
+// @version      1.1
 // @description  Fixes SNOW's annoying new interface
-// @match      https://cern.service-now.com/*
-// @copyright  2015+, Alex Iribarren
-// @downloadURL https://raw.githubusercontent.com/alexiri/snow-fixer/master/snow-fixer.user.js
-// @require http://code.jquery.com/jquery-latest.min.js
+// @match        https://cern.service-now.com/*
+// @copyright    2015+, Alex Iribarren
+// @downloadURL  https://raw.githubusercontent.com/alexiri/snow-fixer/master/snow-fixer.user.js
+// @require 	 http://code.jquery.com/jquery-latest.min.js
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
+    'use strict';
 
-var css = " \
- HTML[data-doctype=true] TD.vt { padding: 0px !important } \
- body { line-height: 1 !important } \
- .table > thead > tr > th, .table > thead > tr > td, .table > tbody > tr > th, .table > tbody > tr > td, .table > tfoot > tr > th, .table > tfoot > tr > td { line-height: 1 !important } \
-";
-
- GM_addStyle(css);
+    GM_addStyle('div.container { width: 99% !important; }');
 })();
