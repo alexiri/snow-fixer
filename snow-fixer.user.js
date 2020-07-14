b786bcbc20acb930c3fa72c10d282430bb824583
Alex Iribarren <alex.iribarren@gmail.com>
2020-07-14 09:59:39 +0200

    Stop opening Tool View in another tab

--- snow-fixer.user.js
+++ snow-fixer.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Service Now Fixer
 // @namespace    http://cern.ch
-// @version      1.2
+// @version      1.3
 // @description  Fixes SNOW's annoying new interface
 // @match        https://cern.service-now.com/*
 // @copyright    2015+, Alex Iribarren
 // @downloadURL  https://raw.githubusercontent.com/alexiri/snow-fixer/master/snow-fixer.user.js
-// @require 	 http://code.jquery.com/jquery-latest.min.js
+// @require 	 https://code.jquery.com/jquery-latest.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant        GM_addStyle
 // ==/UserScript==
 
@@ -17,4 +18,9 @@
     GM_addStyle('.navbar-brand { font-family: "Coming Soon"; }');
 
     GM_addStyle('div.container { width: 99% !important; }');
+    
+    waitForKeyElements('a[ng-if$=openTool]', function() {
+        // Don't open 'Tool view' in another tab
+        $('a[ng-if$=openTool]').attr('target', '_self');
+    });
 })();
